b380a88c71a0bb7a77061d4cfa74fc2b599a93ef
Ricardo Sanchez <ricardo.sanchez@utexas.edu>
2018-03-16 00:44:26 -0500

    Changed tabs to spaces.

--- queue/my_queue.h
+++ queue/my_queue.h
@@ -5,99 +5,99 @@
  * nization; allows multiple readers and a single writer at a time. */
 template <typename T, class C = std::deque<T>>
 class my_queue {
-	C q;
-	pthread_cond_t readers_cv, writers_cv;
-	pthread_mutex_t lock;
-	int num_readers, num_writers;
+    C q;
+    pthread_cond_t readers_cv, writers_cv;
+    pthread_mutex_t lock;
+    int num_readers, num_writers;
 
-	void w_lock() {
-		pthread_mutex_lock(&lock);
-		++num_writers;
-		if(num_writers > 1 || num_readers > 0) {
-			pthread_cond_wait(&writers_cv, &lock);
-		}
-		pthread_mutex_unlock(&lock);
-	}
-	void w_unlock() {
-		pthread_mutex_lock(&lock);
-		--num_writers;
-		if(num_writers > 0) {
-			pthread_cond_signal(&writers_cv);
-		}
-		pthread_cond_broadcast(&readers_cv);
-		pthread_mutex_unlock(&lock);
-	}
+    void w_lock() {
+        pthread_mutex_lock(&lock);
+        ++num_writers;
+        if(num_writers > 1 || num_readers > 0) {
+            pthread_cond_wait(&writers_cv, &lock);
+        }
+        pthread_mutex_unlock(&lock);
+    }
+    void w_unlock() {
+        pthread_mutex_lock(&lock);
+        --num_writers;
+        if(num_writers > 0) {
+            pthread_cond_signal(&writers_cv);
+        }
+        pthread_cond_broadcast(&readers_cv);
+        pthread_mutex_unlock(&lock);
+    }
 
-	void r_lock() {
-		pthread_mutex_lock(&lock);
-		while(num_writers > 0) {
-			pthread_cond_wait(&readers_cv, &lock);
-		}
-		++num_readers;
-		pthread_mutex_unlock(&lock);
-	}
-	void r_unlock() {
-		pthread_mutex_lock(&lock);
-		--num_readers;
-		if(num_readers == 0 && num_writers > 0) {
-			pthread_cond_signal(&writers_cv);
-		}
-		pthread_mutex_unlock(&lock);
-	}
+    void r_lock() {
+        pthread_mutex_lock(&lock);
+        while(num_writers > 0) {
+            pthread_cond_wait(&readers_cv, &lock);
+        }
+        ++num_readers;
+        pthread_mutex_unlock(&lock);
+    }
+    void r_unlock() {
+        pthread_mutex_lock(&lock);
+        --num_readers;
+        if(num_readers == 0 && num_writers > 0) {
+            pthread_cond_signal(&writers_cv);
+        }
+        pthread_mutex_unlock(&lock);
+    }
 
 public:
-	my_queue() : q(), readers_cv(), writers_cv(), lock(), 
-		num_readers(0), num_writers(0) {
-			pthread_cond_init(&readers_cv, NULL);
-			pthread_cond_init(&writers_cv, NULL);
-			pthread_mutex_init(&lock, NULL);
-		}
+    my_queue() : q(), readers_cv(), writers_cv(), lock(), 
+        num_readers(0), num_writers(0) {
+            pthread_cond_init(&readers_cv, NULL);
+            pthread_cond_init(&writers_cv, NULL);
+            pthread_mutex_init(&lock, NULL);
+        }
 
-	void push(const T& value) {
-		w_lock();
-		q.push_front(value);
-		w_unlock();
-	}
+    void push(const T& value) {
+        w_lock();
+        q.push_front(value);
+        w_unlock();
+    }
 
-	T front ()  {
-		T t;
-		if(!q.empty()) {
-			r_lock();
-			t = q.front();
-			r_unlock();
-		}
-		return t;
-	}
+    T front ()  {
+        T t;
+        if(!q.empty()) {
+            r_lock();
+            t = q.front();
+            r_unlock();
+        }
+        return t;
+    }
 
-	T back ()  {
-		T t;
-		if(!q.empty()) {
-			r_lock();
-			t = q.back();
-			r_unlock();
-		}
-		return t;
-	}
+    T back ()  {
+        T t;
+        if(!q.empty()) {
+            r_lock();
+            t = q.back();
+            r_unlock();
+        }
+        return t;
+    }
 
-	void pop() {
-		if(!q.empty()) {
-			w_lock();
-			q.pop_back();
-			w_unlock();
-		}
-	}
+    void pop() {
+        if(!q.empty()) {
+            w_lock();
+            q.pop_back();
+            w_unlock();
+        }
+    }
 
-	bool empty() const {
-		return q.size() == 0;
-	}
+    bool empty() const {
+        return q.size() == 0;
+    }
 
-	int size() const {
-		return q.size();
-	}
-	~my_queue() {
-		pthread_cond_destroy(&readers_cv);
-		pthread_cond_destroy(&writers_cv);
-		pthread_mutex_destroy(&lock);
-	}
+    int size() const {
+        return q.size();
+    }
+    ~my_queue() {
+        pthread_cond_destroy(&readers_cv);
+        pthread_cond_destroy(&writers_cv);
+        pthread_mutex_destroy(&lock);
+    }
 };
 
--- queue/test_my_queue.c++
+++ queue/test_my_queue.c++
@@ -11,39 +11,39 @@
  * many readings to the queue and test its responsiveness
  * to many threads). */
 void *run_test(void* _q) {
-	my_queue<int>* q = ((my_queue<int>*)_q);
-	
-	int counter = std::rand() % 100 + 1;
-	for(int i = 0; i < NUMS_PER_THREAD; ++i, ++counter) {
-		q->push(counter);
-	}
+    my_queue<int>* q = ((my_queue<int>*)_q);
+    
+    int counter = std::rand() % 100 + 1;
+    for(int i = 0; i < NUMS_PER_THREAD; ++i, ++counter) {
+        q->push(counter);
+    }
 
-	for(int i = 0; i < NUMS_PER_THREAD; ++i) {
-		q->front();
-		if(i < NUMS_PER_THREAD - 1) {
-			q->pop();
-		}
-		q->back();
-	}
+    for(int i = 0; i < NUMS_PER_THREAD; ++i) {
+        q->front();
+        if(i < NUMS_PER_THREAD - 1) {
+            q->pop();
+        }
+        q->back();
+    }
 }
 
 int main() {
 
-	pthread_t threads[NUM_THREADS];
-	my_queue<int> q;
-	int rc;
-	void* status;
+    pthread_t threads[NUM_THREADS];
+    my_queue<int> q;
+    int rc;
+    void* status;
 
-	for(int i = 0; i < NUM_THREADS; ++i) {
-		rc = pthread_create(&threads[i], NULL, run_test, (void *)&q);
-	}
+    for(int i = 0; i < NUM_THREADS; ++i) {
+        rc = pthread_create(&threads[i], NULL, run_test, (void *)&q);
+    }
 
-	for(int i = 0; i < NUM_THREADS; ++i) {
-		rc = pthread_join(threads[i], &status);
-	}
+    for(int i = 0; i < NUM_THREADS; ++i) {
+        rc = pthread_join(threads[i], &status);
+    }
 
-	std::cout << "Queue size: " << q.size() << " ... ";
-	std::cout << "front: " << q.front() << " , back: " << q.back() << "\n";
+    std::cout << "Queue size: " << q.size() << " ... ";
+    std::cout << "front: " << q.front() << " , back: " << q.back() << "\n";
 
-	return 0;
+    return 0;
 }
